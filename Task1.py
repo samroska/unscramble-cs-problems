484b9166632a68cd5a6974d01f3e6aa37bef8dcc
staysassysr <staysassylady@gmail.com>
2021-09-22 16:30:02 -0700

    python code problems with source data

---
+++ Task1.py
@@ -0,0 +1,39 @@
+"""
+Read file into texts and calls.
+It's ok if you don't understand how to read files.
+"""
+import csv
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+
+
+"""
+TASK 1:
+How many different telephone numbers are there in the records? 
+Print a message:
+"There are <count> different telephone numbers in the records."
+"""
+def get_phonenumbers(input_list):
+    list_of_numbers = []
+    for item in input_list:
+        list_of_numbers.append(item[0])
+        list_of_numbers.append(item[1])
+    return set(list_of_numbers)
+
+def get_unique_numbers():
+    numbers_in_texts = get_phonenumbers(texts)
+    numbers_in_calls = get_phonenumbers(calls)
+    unique_numbers = numbers_in_texts.union(numbers_in_calls)
+    count = len(unique_numbers)
+    return count
+
+result = get_unique_numbers()
+
+text = 'There are ' + str(result) + ' different telephone numbers in the records.'
+
+print(text)
